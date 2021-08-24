a62d9a1e72b6b87de5cd9d6b41c936478b93a42e
Jose Jimenez Jr <josejimenezjr0@gmail.com>
2021-08-24 17:45:08 -0500

    Update About.js

--- pages/components/About.js
+++ pages/components/About.js
@@ -1,6 +1,4 @@
-const aboutText = `I am a military veteran and Computer Science graduate who took the scenic route into web development.
-  I have always had a passion for learning and problem solving, and I thrive with the challenges that programming provides.
-  In my free time, I enjoy hanging out with my wife and cats, tinkering with home automation, very amateur woodworking, and playing video games.`
+const aboutText = `I am an experienced full stack engineer skilled with NodeJS and Elixir in production. I am a military veteran and Computer Science graduate who is always ready for the next challenge.`
 
 const About = () => (
   <div id="about" className="bg-teal-500 p-8 flex flex-wrap items-center justify-center">
